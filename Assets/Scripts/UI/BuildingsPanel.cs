3a9458857b310daf2abe3c5c1d8f89c6cafd94d1
MaximBaraniuk <mbar@ciklum.com>
2018-09-23 01:54:33 +0300

    Create project. Add assets

---
+++ Assets/Scripts/UI/BuildingsPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class BuildingsPanel : MonoBehaviour
+    {
+        [SerializeField] private Button _closeButton;
+        
+        void Start()
+        {
+            _closeButton.onClick.AddListener(Close);
+        }
+        
+        private void Close()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

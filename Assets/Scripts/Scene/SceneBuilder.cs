175133d01edbbbfe4750a9d14dcb4a8a808268fb
MaximBaraniuk <mbar@ciklum.com>
2018-09-23 02:07:28 +0300

    Create project. Add assets

---
+++ Assets/Scripts/Scene/SceneBuilder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Scene
+{
+    public class SceneBuilder : MonoBehaviour
+    {
+
+        private const string QuadPath = "SceneObjects/Quad";
+        private const string GameAreaPath = "SceneObjects/GameArea";
+        private void Start()
+        {
+            CreateGameArea(25, 25);
+        }
+
+        private static void CreateGameArea(int width, int height)
+        {
+            var quadPrefab = Resources.Load<GameObject>(QuadPath);
+            var gameAreaPrefab = Resources.Load<GameObject>(GameAreaPath);        
+            var area = Instantiate(gameAreaPrefab);
+ //           area.transform.localScale = new Vector3(0.1f * width, 1, 0.1f * height);
+ //           area.transform.localScale = new Vector3(0.1f * width, 1, 0.1f * height);
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    var pos = new Vector3(-width / 2f + 0.5f + i, 0.01f, -height / 2f + 0.5f + j);
+                    var item = Instantiate(quadPrefab, pos, Quaternion.identity);
+                    item.transform.parent = area.transform;
+                }
+            } 
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainPanelControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainPanelControl : MonoBehaviour
+    {
+        [SerializeField] private Button _buildingsMenu;
+        [SerializeField] private GameObject _buildingsPanel;
+
+        void Start()
+        {
+            _buildingsMenu.onClick.AddListener(OpenBuildingsPanel);
+        }
+
+        private void OpenBuildingsPanel()
+        {
+            _buildingsPanel.SetActive(true);
+        }
+    }
+}

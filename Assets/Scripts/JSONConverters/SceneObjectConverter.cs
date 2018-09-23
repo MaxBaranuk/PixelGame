88d5435577bae3b68e52e88286a0aa2fcbf4f351
MaximBaraniuk <mbar@ciklum.com>
2018-09-23 17:02:46 +0300

    Add UI assets. Create some scene models

---
+++ Assets/Scripts/JSONConverters/SceneObjectConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JSONConverters
+{
+    public class SceneObjectConverter: JsonConverter
+    {
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(SceneObject).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {            
+            var jsonObject = JObject.Load(reader);
+//            var typeString = (string)jsonObject.Property(SceneObjectRecord.TYPE_PROPERTY_NAME);
+//            var record = typeString.Match().To<SceneObjectRecord>()
+//                .With(nameof(SceneObjectRecord)).Return(new SceneObjectRecord())
+//                .With(nameof(EditableSceneObjectRecord)).Return(new EditableSceneObjectRecord())
+//                .With(nameof(BayRecord)).Return(new BayRecord())
+//                .Else(_ => { throw new JsonReaderException($"Can't parse SceneObjectRecord, unknown type: \"{typeString}\"."); })
+//                .Result();
+
+            var record = new SceneObject();
+            using (var jsonReader = jsonObject.CreateReader())
+            {
+                serializer.Populate(jsonReader, record);
+            }
+
+            return record;
+        }
+    }
+}
---
+++ Assets/Scripts/Models/SceneObject.cs
@@ -0,0 +1,16 @@
+﻿using JSONConverters;
+using Meta.JsonConverters;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Models
+{
+   [JsonConverter(typeof(SceneObjectConverter))]
+   public class SceneObject
+   {
+      [JsonProperty("position")]
+      private Vector3 _position;
+      [JsonProperty("rotation"), JsonConverter(typeof(QuaternionJsonConverter))]
+      private Quaternion _rotation;
+   }
+}
